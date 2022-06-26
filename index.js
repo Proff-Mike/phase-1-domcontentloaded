c9ea5f902783946688aea7696c20e829e8adf675
Michael Okoe <michael.okoejr@gmail.com>
2022-06-26 13:00:20 +0000

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const newText = document.getElementById('text');
+
+document.addEventListener("DOMContentLoaded", function() {
+  newText.textContent = 'This is so cool!';
+  console.log("The DOM has loaded");
+});
+console.log(text.textContent);
+
+
